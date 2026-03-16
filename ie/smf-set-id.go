ebd22f87b43576b745c8f81bcaa1c5bded25cfef
Kevin Suon <kevin.suon@halys.fr>
2026-03-16 14:56:34 +0100

    fix(ie): add spare octet for smf-set-id IE

--- ie/smf-set-id.go
+++ ie/smf-set-id.go
@@ -4,9 +4,22 @@
 
 package ie
 
+import "github.com/wmnsk/go-pfcp/internal/utils"
+
 // NewSMFSetID creates a new SMFSetID IE.
 func NewSMFSetID(id string) *IE {
-	return newFQDNIE(SMFSetID, id)
+
+	// Warning in the SMF-SET-ID we had the first octet like a spare
+	//   Octets    Bits
+	//   ---------------------------------------------
+	//   1 - 2     Option Type = 180 (decimal)
+	//   3 - 4     Length = n
+	//   5         Spare
+	//   6 - m     FQDN
+	fqdn := utils.EncodeFQDN(id)
+	value := make([]byte, len(fqdn)+1)
+	copy(value[1:], fqdn)
+	return New(SMFSetID, value)
 }
 
 // SMFSetID returns SMFSetID in string if the type of IE matches.
@@ -15,5 +28,19 @@ func (i *IE) SMFSetID() (string, error) {
 		return "", &InvalidTypeError{Type: i.Type}
 	}
 
-	return i.ValueAsFQDN()
+	// Warning in the SMF-SET-ID we had the first octet like a spare
+	//   Octets    Bits
+	//   ---------------------------------------------
+	//   1 - 2     Option Type = 180 (decimal)
+	//   3 - 4     Length = n
+	//   5         Spare
+	//   6 - m     FQDN
+	if len(i.Payload) == 0 || len(i.Payload) == 1 {
+		return "", nil
+	}
+	// If the IE is grouped
+	if i.IsGrouped() {
+		return "", &InvalidTypeError{Type: i.Type}
+	}
+	return utils.DecodeFQDN(i.Payload[1:]), nil
 }
